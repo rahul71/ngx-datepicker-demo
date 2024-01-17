4d7acd51ac1153ba0b858b4eb62a772200d3b064
rahul71 <rahul71@gmail.com>
2024-01-17 17:22:58 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,13 @@
-import { OnInit, AfterViewInit, Component, ViewChild } from '@angular/core';
-import { CalFieldComponent } from './cal-field.component';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-	date2 = new Date('01/01/2024');
-	date3 = new Date('01/01/2024');
+export class AppComponent {
 	title = 'Angular15App';
+	date1:any = new Date();
+	date2:any = new Date();
 	minDate = new Date();
-	maxDate = new Date('01/20/2024');
-	appForm: FormGroup;
-	disabled: boolean = false;
-	required: boolean = false;
-	ngOnInit(): void {
-		this.appForm = new FormGroup({
-			'calField': new FormControl(new Date())
-		});
-	}
-	onSubmit() {
-		console.log(this.appForm.value);
-	}
-	toggleDisableCalField() {
-		if (!this.appForm.controls['calField'].disabled) {
-			this.appForm.controls['calField'].disable();
-		} else {
-			this.appForm.controls['calField'].enable();
-		}
-	}
-	ngModelChangeCalled(val: any) {
-		console.log('ngModelChangeCalled' + val);
-	}
-	toggleValidate() {
-		this.required = !this.required;
-	}
 }
