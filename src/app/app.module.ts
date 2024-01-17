b3738242af4282b2af405a58f4e23955c5deb78f
rahul71 <rahul71@gmail.com>
2024-01-17 17:24:12 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PopoverModule } from 'ngx-bootstrap/popover';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { CalFieldComponent } from './cal-field.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MyMinDateValidatorDirective } from './minDateValidator';
 
 @NgModule({
 	declarations: [
-		AppComponent,
-		CalFieldComponent,
-		MyMinDateValidatorDirective
+		AppComponent
 	],
 	imports: [
 		BrowserModule,
@@ -22,7 +17,6 @@ import { MyMinDateValidatorDirective } from './minDateValidator';
 		FormsModule,
 		ReactiveFormsModule,
 		BsDatepickerModule.forRoot(),
-		PopoverModule.forRoot()
 	],
 	providers: [],
 	bootstrap: [AppComponent]
